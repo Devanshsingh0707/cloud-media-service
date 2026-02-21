38b22b692c785e9e595b7f865f3abef55476ac70
Devansh Singh <dev.devansh.singh007@gmail.com>
2026-02-21 12:55:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use("/api", uploadRoutes);
 
 // 🔥 Root route → Serve login page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/login.html"));
+  res.sendFile(path.join(__dirname, "../public/login_register.html"));
 });
 
 // Server
@@ -39,3 +39,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
